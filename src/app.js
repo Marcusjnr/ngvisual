c6235ec0a1438aa1df56c5d53eaa7987041e5cb9
Okocha Ebube <okochaebube@gmail.com>
2020-02-19 15:02:01 +0100

    added cors correction

--- src/app.js
+++ src/app.js
@@ -14,16 +14,17 @@ app.use(express.json());
 app.use(personnelRoute);
 app.use(patientRoute);
 app.use(caseRoute);
-app.use(cors(), function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", corsConfig.origins);
-    res.header("Access-Control-Allow-Headers", corsConfig.headers);
-    if (req.method === "OPTIONS") {
-        //preflight request
-        res.header("Access-Control-Allow-Methods", corsConfig.methods);
-        return res.status(200).json({});
-    }
-    next();
-});
+app.use(cors());
+// app.use(cors(), function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", corsConfig.origins);
+//     res.header("Access-Control-Allow-Headers", corsConfig.headers);
+//     if (req.method === "OPTIONS") {
+//         //preflight request
+//         res.header("Access-Control-Allow-Methods", corsConfig.methods);
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
 app.listen(port, ()=>{
     console.log('Server is up on port ' + port)
